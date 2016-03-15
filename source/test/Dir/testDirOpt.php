5720266c4e93ce80f8ff8b3b68913cb2125c79f1
beck <cabing2005@gmail.com>
2016-03-15 13:15:58 +0800

    go

--- source/test/Dir/testDirOpt.php
+++ source/test/Dir/testDirOpt.php
@@ -2,8 +2,9 @@
 set_time_limit(0);
 error_reporting(E_ERROR | E_WARNING);
 require_once dirname(dirname(__FILE__)).'/autoload.php';
+require_once dirname(dirname(dirname(__FILE__))).'/Dir/DirOpt.php';
 use php_library\dir;
 
 $path = "/Users/kang/Documents/phpProject/otherproject/php-library/source";
-$obj = new DirOpt();
+$obj = new php_library\dir\DirOpt();
 print_r($obj->getDirList($path));
