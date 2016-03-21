f4d4c345709e3de99ece33a6451119d7907e7d29
beck <cabing2005@gmail.com>
2016-03-21 14:53:37 +0800

    add mccache

---
+++ source/Config/Config.php
@@ -0,0 +1,24 @@
+<?php
+/*
+cusor:
+user:kang
+date:2016年3月21日
+project-name:project_name
+package_name:package_name
+*/
+
+class Config{
+	
+	public static $filecache_config = array("cache_path"=>"/Users/kang/Documents/phpProject/otherproject/php-library/source/Cache/tmp_filecache/");
+	
+	
+	public static $mc_config = array(
+			
+		"default" => array(
+						array('host'=>'127.0.0.1','port'=>'11211')
+						),
+			
+	);
+	
+	
+}
