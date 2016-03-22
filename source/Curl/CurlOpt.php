b18dc5064c3fb01fd03155f876e5b7fe910b884d
beck <cabing2005@gmail.com>
2016-03-22 17:43:52 +0800

    curl

---
+++ source/Curl/CurlOpt.php
@@ -0,0 +1,30 @@
+<?php
+/*
+cusor:模拟http请求
+user:kang
+date:2016年3月22日
+project-name:project_name
+package_name:package_name
+*/
+namespace php_library\curl;
+class CurlOpt{
+	
+	
+	public function get(){
+		
+	}
+	public function post(){
+	
+	}
+	public function put(){
+	
+	}
+	public function delete(){
+	
+	}
+	
+	public function __construct(){
+
+	}
+	
+}
