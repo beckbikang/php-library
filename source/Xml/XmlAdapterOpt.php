95028e9235ee701ef26a1faabae44007d33fe354
beck <cabing2005@gmail.com>
2016-03-25 10:29:15 +0800

    add xml function

---
+++ source/Xml/XmlAdapterOpt.php
@@ -0,0 +1,133 @@
+<?php
+/*
+cusor:
+user:kang
+date:2016年3月24日
+project-name:project_name
+package_name:package_name
+
+we will use these extension
+SimpleXML,DOM,libxml, , SOAP, WDDX, XSL, XML, XMLReader, XMLRPC 和 XMLWriter
+
+*/
+namespace php_library\xml;
+
+class XmlAdapterOptException extends \Exception{}
+class XmlAdapterOpt{
+	private $xml_read_obj;
+	private $xml = "";
+	
+	public function __construct($file=""){
+		if(!empty($file)){
+			if(!is_file($file)){
+				throw new XmlAdapterOptException("file open error");
+			}
+			$this->xml = file_get_contents($file);
+			//$this->xml_read_obj = simplexml_load_file($file);
+		}
+	}
+	public function setXml($str){
+		$this->xml = $str;
+	}
+	
+	public function useSimpleXml(){
+		$this->xml_read_obj = simplexml_load_string($this->xml);
+	}
+	
+	public function useSimpleXmlIterator(){
+		$this->xml_read_obj = new \SimpleXmlIterator($this->xml);
+	}
+	
+	function sxiToArray($sxi){
+		$a = array();
+		for( $sxi->rewind(); $sxi->valid(); $sxi->next() ) {
+			if(!array_key_exists($sxi->key(), $a)){
+				$a[$sxi->key()] = array();
+			}
+			if($sxi->hasChildren()){
+				$a[$sxi->key()][] = sxiToArray($sxi->current());
+			}
+			else{
+				$a[$sxi->key()][] = strval($sxi->current());
+			}
+		}
+		return $a;
+	}
+	
+	public function convertXmlBySimpleXmlIterator(){
+		return $this->convertDoIt($this->xml_read_obj);
+	}
+	//copy from php.net
+	public function convertDoIt($iterator_obj){
+		$xml_arr = array();
+		for($iterator_obj->rewind();$iterator_obj->valid();$iterator_obj->next()){
+			$key = $iterator_obj->key();
+			if(!isset($xml_arr[$key])) $xml_arr[$key] = array();
+			if(!$iterator_obj->hasChildren()){
+				$xml_arr[$key] = strval($iterator_obj->current());
+			}else{
+				$xml_arr[$key][] = $this->convertDoIt($iterator_obj->current());
+			}
+		}
+		return $xml_arr;
+	}
+	
+	public function getDataBySimpleXpath($xpath){
+		$this->useSimpleXml();
+		$ret = $this->xml_read_obj->xpath($xpath);
+
+		if(!empty($ret)){
+			try{
+				$ret = strval($ret[0]);
+			}catch (Exception $e){
+				echo "convert faild";
+			}
+		}
+		return $ret;
+	}
+	
+	public function readDateBySimpleXml(){
+		$this->useSimpleXml();
+	}
+
+	
+	public function createDomObj($file=""){
+		$this->xml_read_obj = new \DOMDocument();
+		if(empty($file)){
+			$this->xml_read_obj->loadXML($this->xml);
+		}
+		
+	}
+	//处理Dom
+	
+	
+	
+	
+	public function __destruct() {
+		$this->xml_read_obj = "";
+		$this->xml = "";
+	}	
+}
+
+$xml = '
+<xml>
+<list>
+<name>tom</name>
+<age>21</age>
+</list>
+<list>
+<name>tim</name>
+<age>31</age>
+</list>
+</xml>';
+
+
+$obj = new XmlAdapterOpt();
+$obj->setXml($xml);
+$xpath ="/xml/list[2]/name";
+$ret = $obj->getDataBySimpleXpath($xpath);
+$obj->useSimpleXmlIterator();
+$arr = $obj->convertXmlBySimpleXmlIterator();
+print_r($arr);
+
+
